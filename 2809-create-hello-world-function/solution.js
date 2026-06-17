68522d052ff0ed80b450cd3c2724df3b21bd9a7e
samarthyamgar <samarthyamgar321@gmail.com>
2026-06-17 16:13:20 +0000

    Sync LeetCode submission Runtime - 42 ms (58.69%), Memory - 55.1 MB (5.02%)

--- 2809-create-hello-world-function/solution.js
+++ 2809-create-hello-world-function/solution.js
@@ -1,11 +1,3 @@
 var createHelloWorld = function() {
-    return function() {
-        return"Hello World";
-    };
+    return () => "Hello World";
 };
-
-const f = createHelloWorld();
-
-// console.log(f());
-console.log(f({},null,42));
-
